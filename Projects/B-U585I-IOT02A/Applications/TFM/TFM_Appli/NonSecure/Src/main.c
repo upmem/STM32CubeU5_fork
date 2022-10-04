f556af7f91da1f5188a1c5e588bd997d1db97fb6
Massimiliano Bartoli <mbartoli@upmem.com>
2022-10-04 17:10:11 +0200

    SEC-19: move system clock and power config in system.c
    Signed-off-by: Massimiliano Bartoli <mbartoli@upmem.com>

--- Projects/B-U585I-IOT02A/Applications/TFM/TFM_Appli/NonSecure/Src/main.c
+++ Projects/B-U585I-IOT02A/Applications/TFM/TFM_Appli/NonSecure/Src/main.c
@@ -41,6 +41,7 @@ __asm("  .global __ARM_use_no_argv\n");
 
 #include "dma.h"
 #include "spi.h"
+#include "system.h"
 #include "error.h"
 
 #include "pilot_config.h"
@@ -68,17 +69,11 @@ volatile uint32_t TestNumber  __attribute__((section(".bss.NoInit"))) ;
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 
-#define SYSCLK_160MHZ
-//#define SYSCLK_80MHZ
-//#define USE_SMPS
-
 #define USER_APP_NBLINKS  ((uint8_t) 1U)
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 
 /* Private function prototypes -----------------------------------------------*/
-static void SystemClock_Config(void);
-static void SystemPower_Config(void);
 static void uart_putc(unsigned char c)
 {
   COM_Transmit(&c, 1, 1000U);
@@ -225,131 +220,7 @@ void my_second_task (void *pvParameters) {
     }
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-#ifdef SYSCLK_160MHZ
-  /** Configure the main internal regulator output voltage
-  */
-  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB busses clocks
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_MSI;
-  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_0;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
-  RCC_OscInitStruct.PLL.PLLMBOOST = RCC_PLLMBOOST_DIV4;
-  RCC_OscInitStruct.PLL.PLLM = 3;
-  RCC_OscInitStruct.PLL.PLLN = 10;
-  RCC_OscInitStruct.PLL.PLLP = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 2;
-  RCC_OscInitStruct.PLL.PLLR = 1;
-  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLLVCIRANGE_1;
-  RCC_OscInitStruct.PLL.PLLFRACN = 0;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB busses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
-                              |RCC_CLOCKTYPE_PCLK3;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB3CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-#elif defined(SYSCLK_80MHZ)
-  /** Configure the main internal regulator output voltage
-  */
-  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB busses clocks
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_MSI;
-  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_0;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
-  RCC_OscInitStruct.PLL.PLLMBOOST = RCC_PLLMBOOST_DIV4;
-  RCC_OscInitStruct.PLL.PLLM = 3;
-  RCC_OscInitStruct.PLL.PLLN = 10;
-  RCC_OscInitStruct.PLL.PLLP = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 2;
-  RCC_OscInitStruct.PLL.PLLR = 2;
-  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLLVCIRANGE_1;
-  RCC_OscInitStruct.PLL.PLLFRACN = 0;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB busses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
-                              |RCC_CLOCKTYPE_PCLK3;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB3CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-#else
-#error "At least SYSCLK_160MHZ or SYSCLK_80MHZ must be defined"
-#endif
-}
-/**
-  * @brief Power Configuration
-  * @retval None
-  */
-static void SystemPower_Config(void)
-{
-  HAL_PWREx_EnableVddIO2();
-
-  /*
-   * Disable the internal Pull-Up in Dead Battery pins of UCPD peripheral
-   */
-  HAL_PWREx_DisableUCPDDeadBattery();
-
-#ifdef USE_SMPS
-  /* Switch to SMPS regulator instead of LDO */
-  if (HAL_PWREx_ConfigSupply(PWR_SMPS_SUPPLY)!= HAL_OK)
-  {
-    Error_Handler();
-  }
-#endif
-
-}
 
 #ifdef  USE_FULL_ASSERT
 /**
---
+++ Projects/B-U585I-IOT02A/Applications/TFM/TFM_Appli/NonSecure/Src/system.c
@@ -0,0 +1,83 @@
+/**
+ *
+ * Copyright (c) 2022 UPMEM.
+ *
+ */
+
+#include "stm32u5xx_hal.h"
+#include "system.h"
+#include "error.h"
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_OscInitStruct.OscillatorType = OSC_INIT_OSCILLATORTYPE;
+  RCC_OscInitStruct.HSI48State = OSC_INIT_HSI48STATE;
+  RCC_OscInitStruct.MSIState = OSC_INIT_MSISTATE;
+  RCC_OscInitStruct.MSICalibrationValue = OSC_INIT_MSICALIBRATION;
+  RCC_OscInitStruct.MSIClockRange = OSC_INIT_MSIRANGE;
+  RCC_OscInitStruct.PLL.PLLState = OSC_INIT_PLLSTATE;
+  RCC_OscInitStruct.PLL.PLLSource = OSC_INIT_PLLSOURCE;
+  RCC_OscInitStruct.PLL.PLLMBOOST = OSC_INIT_PLLMBOOST;
+  RCC_OscInitStruct.PLL.PLLM = OSC_INIT_PLLM;
+  RCC_OscInitStruct.PLL.PLLN = OSC_INIT_PLLN;
+  RCC_OscInitStruct.PLL.PLLP = OSC_INIT_PLLP;
+  RCC_OscInitStruct.PLL.PLLQ = OSC_INIT_PLLQ;
+  RCC_OscInitStruct.PLL.PLLR = OSC_INIT_PLLR;
+  RCC_OscInitStruct.PLL.PLLRGE = OSC_INIT_PLLRGE;
+  RCC_OscInitStruct.PLL.PLLFRACN = OSC_INIT_PLLFRACN;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_ClkInitStruct.ClockType = CLK_INIT_CLOCKTYPE;
+  RCC_ClkInitStruct.SYSCLKSource = CLK_INIT_SYSCLKSOURCE;
+  RCC_ClkInitStruct.AHBCLKDivider = CLK_INIT_AHBCLKDIV;
+  RCC_ClkInitStruct.APB1CLKDivider = CLK_INIT_APB1CLKDIV;
+  RCC_ClkInitStruct.APB2CLKDivider = CLK_INIT_APB2LKDIV;
+  RCC_ClkInitStruct.APB3CLKDivider = CLK_INIT_APB3LKDIV;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+/**
+  * @brief Power Configuration
+  * @retval None
+  */
+void SystemPower_Config(void)
+{
+  HAL_PWREx_EnableVddIO2();
+
+  /*
+   * Disable the internal Pull-Up in Dead Battery pins of UCPD peripheral
+   */
+  HAL_PWREx_DisableUCPDDeadBattery();
+
+#ifdef USE_SMPS
+  /* Switch to SMPS regulator instead of LDO */
+  if (HAL_PWREx_ConfigSupply(PWR_SMPS_SUPPLY)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+#endif
+
+}
