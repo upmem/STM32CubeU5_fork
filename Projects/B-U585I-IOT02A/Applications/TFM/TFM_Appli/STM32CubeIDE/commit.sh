e8841a78780e89f9863914a9bcdfdb5133f6f4e5
Massimiliano Bartoli <mbartoli@upmem.com>
2022-10-26 11:51:03 +0200

    SEC-22: commit.sh fix
    Signed-off-by: Massimiliano Bartoli <mbartoli@upmem.com>

--- Projects/B-U585I-IOT02A/Applications/TFM/TFM_Appli/STM32CubeIDE/commit.sh
+++ Projects/B-U585I-IOT02A/Applications/TFM/TFM_Appli/STM32CubeIDE/commit.sh
@@ -1,7 +1,7 @@
 export commit="$(git log --format='%h' HEAD -n1)"
-export DIRTY=$(git status --porcelain | wc -l)
+export DIRTY=$(git status -uno |  grep "modified:" |  wc -l)
 if [ 0 -eq $DIRTY ]; then
     echo "uint8_t commit[]=\"0x$commit\";" >../../../NonSecure/Inc/commit.h
 else
     echo "uint8_t commit[]=\"0x$commit dirty\";" >../../../NonSecure/Inc/commit.h
-fi 
+fi
