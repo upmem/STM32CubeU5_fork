d579cf4e74257def86ccf1f5b4dda2827e1a021b
Massimiliano Bartoli <mbartoli@upmem.com>
2022-12-06 17:28:41 +0100

    SEC-27: update interrupts priorities
    Signed-off-by: Massimiliano Bartoli <mbartoli@upmem.com>

--- Projects/B-U585I-IOT02A/Applications/TFM/TFM_Appli/NonSecure/Inc/pilot_config.h
+++ Projects/B-U585I-IOT02A/Applications/TFM/TFM_Appli/NonSecure/Inc/pilot_config.h
@@ -6,11 +6,17 @@
 #ifndef __PILOT_CONFIG_H__
 #define __PILOT_CONFIG_H__
 
-#define  PILOT_TICK_IRQ_PRIORITY      (0)
+#define PILOT_PRIORITY_HIGHEST	(0)
+#define PILOT_PRIORITY_LOWEST	(15)
+
+/* Highest priority as STM hal may use tick inside interrupt handlers */
+#define  PILOT_TICK_IRQ_PRIORITY      (PILOT_PRIORITY_HIGHEST)
 #define  PILOT_SPI_IRQ_PRIORITY       (1)
 #define  PILOT_GPDMA_IRQ_PRIORITY     (1)
 #define  PILOT_LPDMA_IRQ_PRIORITY     (1)
-#define  PILOT_PENDSV_IRQ_PRIORITY    (7)
+
+/* Lowest priority to ensure that the context is switched only once in case of nester interrupts*/
+#define  PILOT_PENDSV_IRQ_PRIORITY    (PILOT_PRIORITY_LOWEST)
 
 #define  PILOT_RTOS_SUPPORT
 #endif
